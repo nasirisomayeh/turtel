b008a68fa9c0ba18f236bd047cedb96af25a5564
nasirisomayeh <95909606+nasirisomayeh@users.noreply.github.com>
2021-12-12 18:34:54 +0330

    Update test_git.py

--- test_git.py
+++ test_git.py
@@ -1,1 +1,10 @@
-print("Hello")
+# turtle.py
+import random as rnd
+import turtle
+color = ["red","blue","purple","green","yellow","brown"]
+spiral = turtle.Turtle()
+for i in range(35):
+    spiral.pencolor(color[rnd.randint(0,5)])
+    spiral.forward(i*10)
+    spiral.right(144)
+turtle.done()
